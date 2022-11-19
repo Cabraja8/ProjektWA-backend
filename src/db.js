8dec10a35224bc571c3eb31af1da2d5a3eab5278
Ivan Čabraja <ivancabraja0@gmail.com>
2022-11-19 19:30:53 +0100

    memory storage i prvi get upit koji radi preko backenda

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+import mongo from "mongodb";
 
 let connection_string =
   "mongodb+srv://ivancabi:<xcxOsXEosX92d1h3>@projektwa.aztnwwk.mongodb.net/test";
--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
-// import express from "express";
-// import connect from "./db.js";
-// var connect = require("./db.js");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-Parser";
+import connect from "./db.js";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import storage from "./memory_storage.js";
 
-var express = require("express");
 var app = express();
 const port = 3000;
 
 // app.use(bodyParser.json());
+app.use(cors());
 
 var tempStorage = [];
 
-app.get("/vratisvekorisnike", async (req, res) => {
-  // let db = await connect();
+// app.get("/vratisvekorisnike", async (req, res) => {
+//   // let db = await connect();
 
-  // let cursor = await db.collection("ProjectGroups").find().sort();
-  // let results = await cursor.toArray();
+//   // let cursor = await db.collection("ProjectGroups").find().sort();
+//   // let results = await cursor.toArray();
 
-  // res.send("Hello World!");
-  // res.json(results);
-  console.log(tempStorage);
-  res.send(tempStorage);
-});
+//   // res.send("Hello World!");
+//   // res.json(results);
+//   console.log(tempStorage);
+//   res.send(tempStorage);
+// });
 
-app.post("/dodajKorisnika", (req, res) => {
-  var data = req.body;
-  console.log(data);
-  var dataid = "1234";
+// app.post("/dodajKorisnika", (req, res) => {
+//   var data = req.body;
+//   console.log(data);
+//   var dataid = "1234";
 
-  console.log(data);
-  tempStorage.push(data);
-  res.send(tempStorage);
+//   console.log(data);
+//   tempStorage.push(data);
+//   res.send(tempStorage);
+// });
+
+app.get("/posts", (req, res) => {
+  res.json(storage.posts);
 });
 
 app.listen(port, () => {
