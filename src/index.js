1e845def8c19c518f0eb3b442214247ad629b6bb
Ivan Čabraja <ivancabraja0@gmail.com>
2023-01-19 20:59:43 +0100

    dodan cors fix

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.get("/tajna", [auth.verify], (req, res) => {
   res.json({ message: "Ovo je tajna " + req.jwt.username });
 });
